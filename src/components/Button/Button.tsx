78f37f8b4cda31f73908dc82559bb02027fc0b44
jp-knj <kenji.tomita1996@gmail.com>
2022-04-02 21:51:10 +0900

    :art: update styled button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,29 @@
+import styled from '@emotion/styled';
+
 type Props = {
    onClick: () => void;
 };
 
+const StyledButton = styled.button`
+  font-size: 16px;
+  padding: 8px 16px;
+  border: 1.5px solid #555;
+  border-radius: 100px;
+  background-color: transparent;
+
+  transition: 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #555;
+    color: white;
+  }
+`
+
 export function Button({ onClick }: Props): JSX.Element {
    return (
-      <button type="button" onClick={onClick}>
+      <StyledButton type="button" onClick={onClick}>
          Reset
-      </button>
+      </StyledButton>
    );
 }
