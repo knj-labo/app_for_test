ec98178780a28697e1c05c9d58e1125a54661b87
jp-knj <kenji.tomita1996@gmail.com>
2022-04-02 21:50:10 +0900

    :art: update styled components to separate css

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -17,38 +17,6 @@ const Squares = styled.div`
   width: 315px;
   height: 300px;
   margin: 0 auto;
-
-  & > * {
-    width: 100px;
-    height: 100px;
-    border: 3px solid #555;
-
-    font-size: 32px;
-    text-align: center;
-    line-height: 100px;
-    cursor: pointer;
-    transition: background-color 0.3s;
-
-    &:hover {
-      background-color: #d3cce3;
-    }
-
-    &:nth-child(-n + 3) {
-      border-top: unset;
-    }
-
-    &:nth-last-child(-n + 3) {
-      border-bottom: unset;
-    }
-
-    &:nth-child(3n) {
-      border-right: unset;
-    }
-
-    &:nth-child(3n + 1) {
-      border-left: unset;
-    }
-  }
 `
 
 export const Board = ({
--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,13 +1,46 @@
+import styled from '@emotion/styled';
+
 type Props = {
    index: number;
    marker: 'O' | 'X';
    onClick: (index: number) => void;
 };
 
-export function Square({ onClick, marker, index }: Props): JSX.Element {
+const Div = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 3px solid #555;
+  font-size: 32px;
+  text-align: center;
+  line-height: 100px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #d3cce3;
+  }
+
+  &:nth-child(-n + 3) {
+    border-top: unset;
+  }
+
+  &:nth-last-child(-n + 3) {
+    border-bottom: unset;
+  }
+
+  &:nth-child(3n) {
+    border-right: unset;
+  }
+
+  &:nth-child(3n + 1) {
+    border-left: unset;
+  }
+`
+
+export const Square = ({ onClick, marker, index }: Props): JSX.Element => {
    return (
-      <div role="button" tabIndex={0} key={index} onClick={() => onClick(index)} onKeyPress={() => onClick(index)}>
+      <Div role="button" tabIndex={0} key={index} onClick={() => onClick(index)} onKeyPress={() => onClick(index)}>
          {marker}
-      </div>
+      </Div>
    );
 }
