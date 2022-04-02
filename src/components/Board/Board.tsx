f93d121469085c9fff7476b7bfbcc5313d122714
jp-knj <kenji.tomita1996@gmail.com>
2022-04-02 21:26:46 +0900

    :art: styled componets

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/Button/Button';
 import { Square } from '@/components/Square/Square';
+import styled from '@emotion/styled';
 
 type Props = {
    winner: string;
@@ -10,21 +11,61 @@ type Props = {
    renderStatus: () => JSX.Element;
 };
 
-export function Board({
+const Squares = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 315px;
+  height: 300px;
+  margin: 0 auto;
+
+  & > * {
+    width: 100px;
+    height: 100px;
+    border: 3px solid #555;
+
+    font-size: 32px;
+    text-align: center;
+    line-height: 100px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: #d3cce3;
+    }
+
+    &:nth-child(-n + 3) {
+      border-top: unset;
+    }
+
+    &:nth-last-child(-n + 3) {
+      border-bottom: unset;
+    }
+
+    &:nth-child(3n) {
+      border-right: unset;
+    }
+
+    &:nth-child(3n + 1) {
+      border-left: unset;
+    }
+  }
+`
+
+export const Board = ({
    winner,
    player,
    boards,
    handlePlayClick,
    handleResetClick,
    renderStatus,
-}: Props): JSX.Element {
+}: Props): JSX.Element => {
    return (
       <div>
-         <div>
+         <Squares>
             {boards.map((marker, index) => (
                <Square key={index} index={index} onClick={handlePlayClick} marker={marker} />
             ))}
-         </div>
+         </Squares>
          <div>
             {renderStatus()}
             <Button onClick={handleResetClick} />
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,10 +1,22 @@
+import styled from '@emotion/styled';
 import { Board } from '@/components/Board/Board';
 import { useBoard } from '@/components/Board/useBoard';
 
-export function Game() {
+const Main = styled.main`
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Game = () => {
    const initialValues = Array(9).fill(null);
    const initialPlayer = 'O';
    const initialWinner = '';
    const props = useBoard({ initialValues, initialPlayer, initialWinner });
-   return <Board {...props} />;
+   return (
+       <Main>
+           <Board {...props} />
+       </Main>
+       )
 }
