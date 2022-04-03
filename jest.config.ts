9085700a04fd20284eba956633d72563aea3de14
jp-knj <kenji.tomita1996@gmail.com>
2022-04-03 17:35:56 +0900

    :construction: update them for snapshot

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,20 @@
 import type {Config} from '@jest/types';
 
-const esModules = ['', '', ''].join('|');
+const esModules = ['@storybook/addon-docs',].join('|');
 
 const config: Config.InitialOptions = {
     verbose: true,
+    preset: 'ts-jest',
     testEnvironment: 'jsdom',
     transform: {
         "\\.[jt]sx?$": 'esbuild-jest',
-        '\\.(css|scss)$': 'jest-transform-stub'
+        '^.+\\.js$': 'babel-jest',
+        '\\.(css|scss)$': 'jest-transform-stub',
+        "^.+\\.mdx?$": "@storybook/addon-docs/jest-transform-mdx",
     },
     transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+    moduleNameMapper: {
+        "^@/(.+)": "<rootDir>/src/$1"
+    }
 };
 export default config;
