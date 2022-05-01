f4598a0842c98f1385f61a32a4495cde1ca0da29
jp-knj <kenji.tomita1996@gmail.com>
2022-05-01 17:37:24 +0900

    :rocket: update dist path

--- webpack.base.js
+++ webpack.base.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./src/index.tsx",
     output: {
         filename: "index.js",
-        path: path.resolve(__dirname, "build"),
+        path: path.resolve(__dirname, "public"),
     },
     resolve: {
         extensions: [".ts", ".tsx", ".js", ".json"],
