8944169dc387de4b17fb335a8c7f41738f860c25
jp-knj <kenji.tomita1996@gmail.com>
2022-05-04 11:45:08 +0900

    :sparkles: pass test for button

--- src/shared/Button.test.tsx
+++ src/shared/Button.test.tsx
@@ -1,9 +1,11 @@
 import { Button } from './Button'
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect';
 
 describe('should render', () => {
     it('', () => {
         render(<Button onClick={() => null} />)
-        expect(screen.getByRole("button"))
+        const buttonElement = screen.getByRole("button")
+        expect(buttonElement).toHaveTextContent('Reset')
     })
 });
