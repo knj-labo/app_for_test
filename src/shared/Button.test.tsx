877611a6b42dfdea83816533447895a1a7556788
jp-knj <kenji.tomita1996@gmail.com>
2022-05-07 12:49:10 +0900

    :green_heart: add test using react testing library for interaction

--- src/shared/Button.test.tsx
+++ src/shared/Button.test.tsx
@@ -1,11 +1,21 @@
 import { Button } from './Button'
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
 
-describe('should render', () => {
-    it('', () => {
-        render(<Button onClick={() => null} />)
+describe('ボタンのコンポーネントの場合で', () => {
+    const mockOnClick = jest.fn();
+    beforeEach(() =>{
+        render(<Button onClick={mockOnClick} />)
+    })
+    const BUTTON_TEXT = 'Reset';
+    it(`描画されたとき、テキストは ${BUTTON_TEXT}`, () => {
         const buttonElement = screen.getByRole("button")
-        expect(buttonElement).toHaveTextContent('Reset')
+        expect(buttonElement).toHaveTextContent(BUTTON_TEXT)
+    })
+    it(`クリックされたとき、一度だけ呼び出しされる`, () => {
+        expect(mockOnClick).toHaveBeenCalledTimes(0);
+        const buttonElement = screen.getByRole("button")
+        fireEvent.click(buttonElement);
+        expect(mockOnClick).toHaveBeenCalledTimes(1);
     })
 });
