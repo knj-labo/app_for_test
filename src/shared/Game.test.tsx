252e85132036d0a85aa831871a8beca15f3ecac8
jp-knj <kenji.tomita1996@gmail.com>
2022-05-08 00:28:15 +0900

    :green_heart: display some text when clicked each component

--- src/shared/Game.test.tsx
+++ src/shared/Game.test.tsx
@@ -4,9 +4,12 @@ import '@testing-library/jest-dom/extend-expect';
 import { Game } from './Game'
 
 describe('Game のコンポーネントの場合で', () => {
-    it('9つのあるsquareコンポーネントの初期値が適切に表示される', async () => {
+    beforeEach(() => {
         render(<Game />)
-
+    })
+    const NEXT_PLAYER_IS_O = 'Next player: O'
+    const NEXT_PLAYER_IS_X = 'Next player: X'
+    it('9つのあるsquareコンポーネントの初期値が適切に表示される', async () => {
         const squares = screen.queryAllByTestId('square')
         expect(squares.length).toBe(9)
 
@@ -20,39 +23,72 @@ describe('Game のコンポーネントの場合で', () => {
         expect(squares[7].textContent).toBe('')
         expect(squares[8].textContent).toBe('')
 
-        expect(screen.getByTestId('status').textContent).toBe('Next player: O')
+        expect(screen.getByTestId('status').textContent).toBe(NEXT_PLAYER_IS_O)
     })
     const WINNER_IS_O_PLAYER_TEXT = 'Winner: O';
     const WINNER_IS_X_PLAYER_TEXT = 'Winner: X';
-    it(`Oが勝利条件を満たしたとき、${WINNER_IS_O_PLAYER_TEXT}` , async () => {''
-        render(<Game />)
+    describe('勝利条件を満たしたときで', () => {
+        it(`プレイヤーはOなら${WINNER_IS_O_PLAYER_TEXT}と表示される` , async () => {''
+            const squares = screen.queryAllByTestId('square')
 
+            fireEvent.click(squares[0])
+            fireEvent.click(squares[1])
+            fireEvent.click(squares[4])
+            fireEvent.click(squares[2])
+            fireEvent.click(squares[8])
+
+            await waitFor(() => {
+                expect(screen.getByTestId('status').textContent).toBe(WINNER_IS_O_PLAYER_TEXT);
+            })
+        })
+        it(`プレイヤーはXなら${WINNER_IS_X_PLAYER_TEXT}と表示される` , async () => {''
+            const squares = screen.queryAllByTestId('square')
+
+            fireEvent.click(squares[0])
+            fireEvent.click(squares[3])
+            fireEvent.click(squares[6])
+            fireEvent.click(squares[4])
+            fireEvent.click(squares[7])
+            fireEvent.click(squares[5])
+
+            await waitFor(() => {
+                expect(screen.getByTestId('status').textContent).toBe(WINNER_IS_X_PLAYER_TEXT);
+            })
+        })
+    })
+    const DRAW_TEXT = 'Draw';
+    it('引き分けの場合なら', async () => {
         const squares = screen.queryAllByTestId('square')
 
         fireEvent.click(squares[0])
-        fireEvent.click(squares[1])
         fireEvent.click(squares[4])
-        fireEvent.click(squares[2])
         fireEvent.click(squares[8])
-
-        await waitFor(() => {
-            expect(screen.getByTestId('status').textContent).toBe(WINNER_IS_O_PLAYER_TEXT);
-        })
-    })
-    it(`Xが勝利条件を満たしたとき、${WINNER_IS_X_PLAYER_TEXT}` , async () => {''
-        render(<Game />)
-
-        const squares = screen.queryAllByTestId('square')
-
-        fireEvent.click(squares[0])
-        fireEvent.click(squares[3])
         fireEvent.click(squares[6])
-        fireEvent.click(squares[4])
-        fireEvent.click(squares[7])
+        fireEvent.click(squares[2])
         fireEvent.click(squares[5])
+        fireEvent.click(squares[3])
+        fireEvent.click(squares[1])
+        fireEvent.click(squares[7])
 
         await waitFor(() => {
-            expect(screen.getByTestId('status').textContent).toBe(WINNER_IS_X_PLAYER_TEXT);
-        })
+            expect(screen.getByTestId('status').textContent).toBe(DRAW_TEXT);
+        });
     })
+    it(`クリック後にプレイヤーは入れ替わり、${NEXT_PLAYER_IS_X}と${NEXT_PLAYER_IS_O}が交互に表示される`, async () => {
+        const squares = screen.queryAllByTestId('square')
+        expect(screen.getByTestId('status').textContent).toBe(NEXT_PLAYER_IS_O);
+        fireEvent.click(squares[0])
+        await waitFor(() => {
+            expect(screen.getByTestId('status').textContent).toBe(NEXT_PLAYER_IS_X);
+        });
+        fireEvent.click(squares[4])
+        await waitFor(() => {
+            expect(screen.getByTestId('status').textContent).toBe(NEXT_PLAYER_IS_O);
+        });
+        fireEvent.click(squares[8])
+        await waitFor(() => {
+            expect(screen.getByTestId('status').textContent).toBe(NEXT_PLAYER_IS_X);
+        });
+    })
+
 })
