8a3e85c1977e2269db489a0bd38d490be0328971
jp-knj <kenji.tomita1996@gmail.com>
2022-05-08 00:12:59 +0900

    :green_heart: pass test to display text for winner

--- src/shared/Game.test.tsx
+++ src/shared/Game.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect';
 
 import { Game } from './Game'
@@ -21,5 +21,38 @@ describe('Game のコンポーネントの場合で', () => {
         expect(squares[8].textContent).toBe('')
 
         expect(screen.getByTestId('status').textContent).toBe('Next player: O')
+    })
+    const WINNER_IS_O_PLAYER_TEXT = 'Winner: O';
+    const WINNER_IS_X_PLAYER_TEXT = 'Winner: X';
+    it(`Oが勝利条件を満たしたとき、${WINNER_IS_O_PLAYER_TEXT}` , async () => {''
+        render(<Game />)
+
+        const squares = screen.queryAllByTestId('square')
+
+        fireEvent.click(squares[0])
+        fireEvent.click(squares[1])
+        fireEvent.click(squares[4])
+        fireEvent.click(squares[2])
+        fireEvent.click(squares[8])
+
+        await waitFor(() => {
+            expect(screen.getByTestId('status').textContent).toBe(WINNER_IS_O_PLAYER_TEXT);
+        })
+    })
+    it(`Xが勝利条件を満たしたとき、${WINNER_IS_X_PLAYER_TEXT}` , async () => {''
+        render(<Game />)
+
+        const squares = screen.queryAllByTestId('square')
+
+        fireEvent.click(squares[0])
+        fireEvent.click(squares[3])
+        fireEvent.click(squares[6])
+        fireEvent.click(squares[4])
+        fireEvent.click(squares[7])
+        fireEvent.click(squares[5])
+
+        await waitFor(() => {
+            expect(screen.getByTestId('status').textContent).toBe(WINNER_IS_X_PLAYER_TEXT);
+        })
     })
 })
